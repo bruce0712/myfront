2e698777d1c729f7f66efdd9e774a6a70f169b32
bruce0712 <oncwnuOejlmITVqhW5zXTOghOpBo@git.weixin.qq.com>
2021-02-25 18:12:03 +0800

    commit

--- 面试题/MyPromise.js
+++ 面试题/MyPromise.js
@@ -1,35 +1,3 @@
-## Promise原理
-
-**Promise**对象用于表示一个异步操作的最终完成或失败及其结果
-
-一个`Promise` 对象必然处于以下状态之一
-
-- 待定(peding):初始化状态，既没有被兑现，也没有被拒绝。
-- 已兑现(fulfilled):意味着操作成功完成。
-- 已拒绝(rejected):意味着操作失败
-
-#### Promise.resolve的特点：
-
-- 如果参数是一个Promsie实例，那么Promise.resolve将不做任何修改，原封不动的返回这个实例
-- 如果参数是一个原始值，或者是一个不具有then方法的对象,则Promise.resolve方法返回一个新的Promise对象，状态为resolved
-
-#### Promise.all的特点：
-
-- Promise.all方法接受一个数组作为参数，p1、p2、p3都是Promise实例，如果不是，就会调用Promise.resolve方法，将参数转为Promise实例，再进一步处理。
-- 返回值组成一个数组
-- p1、p2、p3中有一个被rejected,p的状态就变成rejected,此时第一个被reject的实例的返回值会传递给p的回调函数
-
-**Promise.race的特点:**
-
-- Promise.race方法的参数与Promise.all方法一样，如果不是Promise实例，就会调用Promise.resolve方法，将参数转为Promise实例，再进一步处理。
-- 返回那个率先改变的Promise实例的返回值
-
-**Promise.allSettled的特点：**
-
-- Promise.allSettled方法接受一个数组作为参数，p1、p2、p3都是Promise实例，如果不是，就会调用Promise.resolve方法，将参数转为Promise实例，再进一步处理。
-- 将p1、p2、p3处理的结果返回出来
-
-```js
 class MyPromise {
     constructor(fn){
         /**
@@ -200,16 +168,31 @@ class MyPromise {
         })
     }
 
-     finally(callBcak){
-        return  this.then(
-            value => MyPromise.resolve(callBcak()).then(()=>value),
-            reason => MyPromise.reject(callBcak()).then(()=>  {throw reason})
-        )
+    finally(callBcak){
+        return  new MyPromise((resolve,reject)=>{
+            this.then((sucessValue)=>{
+                return MyPromise.resolve(callBcak()).then(
+                    (r)=>{
+                        resolve(sucessValue)
+                    }
+                )
+            },(fieldValue => {
+                return MyPromise.reject(callBcak()).then(
+                    null,
+                    e => {
+                        if(e) {
+                            reject(e)
+                        }else {
+                            reject(fieldValue)
+                        }
+                    }
+                )
+            }))
+        })
     }
 
 }
 
-//------ test
 var p = new MyPromise((resolve, reject) => {
     setTimeout(() => {
         console.log('resolve');
@@ -226,10 +209,19 @@ p.then(data => {
     console.log('data2', data2);
 }).catch(err => {
     console.error('err', err);
-}).finally(cb => {
-    console.log('finally',cb)
+})
+
+
+const q1 = new MyPromise((resolve, reject) => {
+    resolve('hello')
 });
-```
 
-
-
+const q2 = new MyPromise((resolve, reject) => {
+    reject('world')
+});
+const q3 = new MyPromise((resolve, reject) => {
+    resolve('hahaha')
+});
+MyPromise.allSettled([q1, q2,q3]).then(res => {
+    console.log(res); // [ 'hello', 'world' ]
+});
